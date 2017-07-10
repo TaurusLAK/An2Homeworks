811ee5c771d2983de146f1350d96fadb68a64a9d
Andrii Luhanin <Andrii_Luhanin@epam.com>
2017-07-10 15:12:25 +0300

    homework_1: Added plain fields to AppComponent

--- shop/src/app/app.component.ts
+++ shop/src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  name: string = "Lorem ipsum dolor sit amet.";
+  description: string = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sed aliquam metus. Suspendisse feugiat diam nisi, vitae pretium magna mattis vel. Nam laoreet ante eu magna convallis, euismod rhoncus est ornare. Morbi vestibulum, sem eget commodo elementum, tortor mauris feugiat lectus, ullamcorper pulvinar erat odio sit amet arcu. Integer eu scelerisque arcu. Donec suscipit arcu nunc, in egestas sapien finibus in. Praesent nisl tellus, fermentum et lorem non, consequat sodales ligula. Nunc porttitor aliquet ligula. In porta pulvinar erat. Integer venenatis hendrerit massa vitae auctor. Sed vel dui sollicitudin, accumsan massa ut, lacinia turpis. Pellentesque vel maximus dolor";
+  price: number = 100;
+  category: Category = Category.ELECTRONIC;
+  isAvailable: boolean = true;
+}
+
+enum Category {
+  ELECTRONIC, MISCELLANEOUS
 }
